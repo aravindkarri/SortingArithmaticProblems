d1ea441e07a45d7eb7d783e0d53f72accba9a735
aravindkarri <aravindkarri98495@gmail.com>
2021-06-12 13:58:33 +0530

    Add: code to compute a*b+c

--- sortingArithmatic.sh
+++ sortingArithmatic.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome to Sorting Arithmatic Problem"
+
 read -p "Enter value for a: " a
 read -p "Enter value for b: " b
 read -p "Enter value for c: " c
 
-result=$(($a+$b*$c))
-echo $result
+n1=$(($a+$b*$c))
+n2=$(($a*$b+$c))
+
+echo $n2
