af3d9c8b20b9880cd203e5f2d6f2800b22cb470d
aravindkarri <aravindkarri98495@gmail.com>
2021-06-12 14:12:17 +0530

    Add: code to store the results in dictionary

--- sortingArithmatic.sh
+++ sortingArithmatic.sh
@@ -6,9 +6,11 @@ read -p "Enter value for a: " a
 read -p "Enter value for b: " b
 read -p "Enter value for c: " c
 
-n1=$(($a+$b*$c))
-n2=$(($a*$b+$c))
-n3=$(($c+$a/$b))
-n4=$(($a%$b+$c))
+declare -A dict
 
-echo $n1 $n2 $n3 $n4
+dict["n1"]=$(($a+$b*$c))
+dict["n2"]=$(($a*$b+$c))
+dict["n3"]=$(($c+$a/$b))
+dict["n4"]=$(($a%$b+$c))
+
+echo ${dict[@]}
